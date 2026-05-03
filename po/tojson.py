99f2e6fb474a671d3707b0e856fc0a3387c8fe3d
Sam Bélanger <astromangamingofficial@gmail.com>
2026-05-03 19:10:39 -0400

    Fix

--- po/tojson.py
+++ po/tojson.py
@@ -1,25 +1,44 @@
-from translate.storage import po
+#!/usr/bin/env python3
+from pathlib import Path
 import sys
-import os
 import json
 import glob
-res = dict()
 
+# Définit le dossier 'po' relatif à l'emplacement du script
+HERE = Path(__file__).resolve().parent
+PO_DIR = HERE
 
-for file in glob.glob('po/*.po'):
-    print("Processing ",file)
-    lang = os.path.basename(file).replace('.po','')
-    fd = open(file,'rb')
-    store = po.pofile()
-    store.parse(fd)
-    d = dict()
-    for k in store.getids():
-        d[k] = store.translate(k)
-    res[lang] = d
-    fd.close()
+# Vérifier la dépendance translate.storage.po
+try:
+    from translate.storage import po
+except ModuleNotFoundError:
+    print("Erreur: module 'translate' introuvable. Installez translate-toolkit :")
+    print("  python3 -m pip install --user translate-toolkit")
+    sys.exit(2)
 
-with open('po/messages.js','w', encoding='utf8') as js:
+res = {}
+
+# Cherche les fichiers .po dans le dossier po (adjacent)
+po_files = sorted(PO_DIR.glob("*.po"))
+if not po_files:
+    print(f"Aucun fichier .po trouvé dans {PO_DIR}")
+else:
+    for file in po_files:
+        print("Processing", file)
+        lang = file.stem
+        with file.open('rb') as fd:
+            store = po.pofile()
+            store.parse(fd)
+            d = {}
+            for k in store.getids():
+                d[k] = store.translate(k)
+            res[lang] = d
+
+# Écrit le fichier messages.js dans le même dossier po
+out_path = PO_DIR / 'messages.js'
+with out_path.open('w', encoding='utf8') as js:
     js.write('var i18n_dicts = ')
-    json.dump(res,js,indent=4,ensure_ascii=False)
+    json.dump(res, js, indent=4, ensure_ascii=False)
     js.write('\n')
 
+print("Wrote", out_path)
